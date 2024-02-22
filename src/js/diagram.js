835c9e396939b449fda5e3299cbea8751df55242
Hanin Farhan <haninfarhan96@gmail.com>
2024-02-22 12:04:49 +0100

    skapat funktion som filtrerar, sorterar och loopar igenom för att skriva ut värden

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -22,3 +22,44 @@ async function getData() {
         console.log("Det gick inte att hämta data");
     }
 }
+
+
+async function displayChartCourses() {
+    //Deklararar variabel som hämtas från html id element
+    const coursesDisplay = document.getElementById("diagram-stapel");
+
+    //Elementet töms
+    coursesDisplay.innerHTML = "";
+
+
+    //Data hämtas från getData funktion
+    let coursesData = await getData();
+
+    //Datan filtreras så vi får ut data som överensstämmer med kurs från api mha filter
+    let filterCourses = coursesData.filter((admission) => admission.type == "Kurs");
+
+    //Datan sorteras så vi får ut störst antal sökande mha sort
+    const sortCourses = filterCourses.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+
+    //De 6 högsta värden plockas ut mha splice
+    const topCourses = sortCourses.slice(0, 6);
+
+    //Två tomma arrays skapas, här ska de nya värden för kurs och sökande sättas in
+    const courseLabels = [];
+    const courseCounts = [];
+
+    //Loopar igenom
+    for (let i = 0; i < topCourses.length; i++) {
+
+    //Om textsträngen är längre än 35 så ska den kapas till 35 tecken + ... för att visa texten fortsätter
+        if (topCourses[i].name.length > 35) {
+            topCourses[i].name = topCourses[i].name.slice(0, 35) + "...";
+        }
+
+    //Lägger in värden i elementen mha push
+        courseLabels.push(topCourses[i].name);
+        courseCounts.push(topCourses[i].applicantsTotal);
+    }
+
+}
+
